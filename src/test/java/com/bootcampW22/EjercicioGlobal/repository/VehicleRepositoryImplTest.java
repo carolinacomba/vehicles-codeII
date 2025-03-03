e90d77d46e9bfd3ad8c4cc00cff9b2f73fafaf06
carolinacomba <carolinacomba42@gmail.com>
2025-03-02 21:39:42 -0300

    test unitarios del repositorio terminados

--- src/test/java/com/bootcampW22/EjercicioGlobal/repository/VehicleRepositoryImplTest.java
+++ src/test/java/com/bootcampW22/EjercicioGlobal/repository/VehicleRepositoryImplTest.java
@@ -22,13 +22,71 @@ class VehicleRepositoryImplTest {
     }
 
     @Test
-    void testFindVehiclesByBrand() {
+    void findAllTest() {
+        List<Vehicle> expected = new ArrayList<>(
+                List.of(new Vehicle(1L, "Ford", "Mustang", "AB123CD", "Red", 2021, "250", 4, "gasoline", "automatic", 140.0, 191.0, 1680.0),
+                        new Vehicle(2L, "Chevrolet", "Camaro", "AC123AB", "Red", 2020, "290", 4, "gasoline", "automatic", 145.0, 198.0, 1750.0),
+                        new Vehicle(3L, "Fiat", "Cronos", "AA123AN", "Blue", 2015, "200", 3, "gas", "manual", 130.0, 191.0, 1580.0),
+                        new Vehicle(4L, "Tesla", "Model S", "AF900DF", "Black", 2017, "210", 4, "gas", "manual", 140.0, 190.0, 1680.0),
+                        new Vehicle(5L, "BMW", "X5", "AD567HH", "White", 2022, "240", 5, "gas", "automatic", 150.0, 151.0, 1180.0))
+        );
+
+        List<Vehicle> result = mockRepository.findAll();
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void findVehiclesByBrandTest() {
         String brand = "Ford";
         List<Vehicle> expected = new ArrayList<>(List.of(
-                new Vehicle(1L, brand, "Mustang", "AB123CD", "Red", 2022, "250", 4, "gasoline", "automatic", 140.3, 191.5, 1680.5)
+                new Vehicle(1L, brand, "Mustang", "AB123CD", "Red", 2021, "250", 4, "gasoline", "automatic", 140.0, 191.0, 1680.0)
         ));
 
         List<Vehicle> result = mockRepository.findVehiclesByBrand(brand);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void findVehiclesByYearAndColorTest() {
+        String color = "Red";
+        int year = 2020;
+
+        List<Vehicle> expected = new ArrayList<>(List.of(
+                new Vehicle(2L, "Chevrolet", "Camaro", "AC123AB", color, year, "290", 4, "gasoline", "automatic", 145.0, 198.0, 1750.0)
+        ));
+
+        List<Vehicle> result = mockRepository.findVehiclesByYearAndColor(color, year);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void findVehiclesByBrandAndRangeOfYearTest(){
+        String brand = "Fiat";
+        int starYear = 2000;
+        int endYear = 2020;
+
+        List<Vehicle> expected = new ArrayList<>(List.of(
+                new Vehicle(3L, brand, "Cronos", "AA123AN", "Blue", 2015, "200", 3, "gas", "manual", 130.0, 191.0, 1580.0)
+        ));
+
+        List<Vehicle> result = mockRepository.findVehiclesByBrandAndRangeOfYear(brand, starYear, endYear);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void findVehiclesByRangeOfWeightTest() {
+        double weightMin = 1100.0;
+        double weightMax = 1200.0;
+
+        List<Vehicle> expected = new ArrayList<>(List.of(
+                new Vehicle(5L, "BMW", "X5", "AD567HH", "White", 2022, "240", 5, "gas", "automatic", 150.0, 151.0, 1180.0)
+        ));
+
+        List<Vehicle> result = mockRepository.findVehiclesByRangeOfWeight(weightMin, weightMax);
 
         assertEquals(expected, result);
     }
